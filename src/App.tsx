71a57760d51482bc8811d0fe233d702ffb1fd8b8
Mohamed Hajjouji <devmohamedhajjouji@gmail.com>
2025-05-05 20:11:10 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Hero from './components/Hero'
 import Features from './components/Features'
 import CallToAction from './components/CallToAction'
 import Footer from './components/Footer'
-import './app.css'
+import "./index.css"
 
 function App() {
 
