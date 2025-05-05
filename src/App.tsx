ddb719b779088315844e21908ea1a2727c638028
Mohamed Hajjouji <devmohamedhajjouji@gmail.com>
2025-05-05 19:18:13 +0100

    testing deployment to vercel with tailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import Hero from './components/Hero'
 import Features from './components/Features'
 import CallToAction from './components/CallToAction'
 import Footer from './components/Footer'
-import './index.css'
+import './app.css'
 
 function App() {
 
 
   return (
-    <div>
+    <div className="font-rubik">
       <Header />
     <div className='px-4 md:px-[100px] max-w-[1440px] mx-auto'>
 
